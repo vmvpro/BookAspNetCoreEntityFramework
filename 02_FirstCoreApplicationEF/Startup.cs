8b5dd05672442dee7de352dcafc97e5b622b3173
vmvpro <melnvetal@gmail.com>
2020-05-02 05:30:24 +0300

    Startup() Configuration

--- 02_FirstCoreApplicationEF/Startup.cs
+++ 02_FirstCoreApplicationEF/Startup.cs
@@ -2,34 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FirstCoreApplicationEF.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FirstCoreApplicationEF
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public Startup(IConfiguration config) => Configuration = config;
+
+		public IConfiguration Configuration { get; }
+
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddMvc();
+			string conString = Configuration["ConnectionStrings:DefaultConnection"];
+			services.AddDbContext<DataContext>(options =>
+				options.UseSqlServer(conString));
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-
-			app.Run(async (context) =>
-			{
-				context.Response.Headers.Add("Content-Type", "text/html;charset=utf-8");
-				await context.Response.WriteAsync("ВМВ");
-			});
+			app.UseDeveloperExceptionPage();
+			app.UseStatusCodePages();
+			app.UseStaticFiles();
+			app.UseMvcWithDefaultRoute();
 		}
 	}
 }
