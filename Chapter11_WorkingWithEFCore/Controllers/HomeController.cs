4a874c44d109ca0637dbc7c6ba60683778bfb0b5
vmvpro <melnvetal@gmail.com>
2020-05-07 18:06:43 +0300

    Chapter11_WorkingWithEFCore Add

---
+++ Chapter11_WorkingWithEFCore/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Chapter11_WorkingWithEFCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// ReSharper disable once CheckNamespace
+namespace Chapter11_WorkingWithEFCore
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View(new Product[] {
+				new Product {   Name = "PI",    Category = "Catl",  Price = 10  },
+				new Product {   Name = "P2",    Category = "Cat2",  Price = 20  },
+				new Product {   Name = "P3",    Category = "Cat3",  Price = 30  }
+			});
+		}
+	}
+}
---
+++ Chapter11_WorkingWithEFCore/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Chapter11_WorkingWithEFCore.Models {
+
+    public class Product {
+
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
